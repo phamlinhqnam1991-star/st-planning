6f5ea454ec3a596656c1a60e98319f9024804bbd
Pham Linh <linh09dt@gmail.com>
2026-09-02 16:36:42 +0700

    Mô tả ngắn gọn công việc vừa làm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,5 @@
-import { redirect } from "next/navigation";
-export default function Page(){redirect("/master-data")}
+import {redirect} from "next/navigation";
+
+export default function Page(){
+ redirect("/dashboard");
+}
